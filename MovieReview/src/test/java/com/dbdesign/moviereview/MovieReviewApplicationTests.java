455e995f1658fc1e4c736923fc4680bc7ed747ce
anismynm <ddoe0318@naver.com>
2024-11-26 14:07:19 +0900

    초기 설정

---
+++ MovieReview/src/test/java/com/dbdesign/moviereview/MovieReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbdesign.moviereview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
